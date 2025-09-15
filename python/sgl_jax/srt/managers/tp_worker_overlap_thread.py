4a297ccfe19624f3da9b06d74c1cf75f1aeac7b7
pc-new <pengchengneo@gmail.com>
2025-09-15 15:46:24 +0800

    add log

--- python/sgl_jax/srt/managers/tp_worker_overlap_thread.py
+++ python/sgl_jax/srt/managers/tp_worker_overlap_thread.py
@@ -26,6 +26,8 @@ logger = logging.getLogger(__name__)
 
 @jax.jit
 def resolve_future_token_ids(input_ids, future_token_ids_map):
+    logger.info(f"---------------{input_ids}")
+    logger.info(f"***************{future_token_ids_map}")
     return jnp.where(
         input_ids < 0,
         future_token_ids_map[jnp.clip(-input_ids, a_min=0)],
