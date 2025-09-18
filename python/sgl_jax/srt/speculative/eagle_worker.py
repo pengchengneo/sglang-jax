ae5098ffb0e305a75cf00c7b59fb0e0d7b60d883
pc-new <pengchengneo@gmail.com>
2025-09-18 11:07:19 +0800

    fix bug

--- python/sgl_jax/srt/speculative/eagle_worker.py
+++ python/sgl_jax/srt/speculative/eagle_worker.py
@@ -80,6 +80,9 @@ class EAGLEWorker(ModelWorker):
         self, model_worker_batch: ModelWorkerBatch, sample_meta_data: SamplingMetadata
     ) -> Tuple[LogitsProcessorOutput, jax.Array, int, int, np.ndarray]:
         model_worker_batch.capture_hidden_mode = CaptureHiddenMode.FULL
+        logger.info(
+            f"-------------forward_target_extend------------11111-{model_worker_batch.seq_lens} {model_worker_batch.input_ids}"
+        )
         logits_output, next_token_ids, cache_miss_count = (
             self.target_worker.forward_batch_generation(
                 model_worker_batch, sampling_metadata=sample_meta_data
